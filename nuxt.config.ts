e42b386209f7fd455704d205428d378482f9de06
DESKTOP-0B1K1KI\My <atiwattum1@gmai.com>
2025-05-09 23:14:11 +0700

    test project

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: false },
+  build: {
+    transpile: ['vuetify'],
+  },
+  css: [
+    'vuetify/styles',
+    '@mdi/font/css/materialdesignicons.css',
+  ],
+   modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  app: {
+    baseURL: '/test-web/',
+  },
+  nitro: {
+    preset: 'static',
+  },
+})
